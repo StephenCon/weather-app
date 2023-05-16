b2576adbe8dd98a32f3b5446e5d2f7be33268917
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-16 16:30:46 +0100

    implemented search bar autocomplete

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,20 @@
 import React from 'react';
+const google = window.google;
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+    this.searchInputRef = React.createRef();
     this.state = { term: '' };
+  }
+
+  componentDidMount() {
+    this.searchAutocomplete = new google.maps.places.Autocomplete(this.searchInputRef.current);
+
+    this.searchAutocomplete.addListener('place_changed', () => {
+      const place = this.searchAutocomplete.getPlace();
+      this.setState({ term: place.name });
+    });
   }
 
   onFormSubmit = (event) => {
@@ -16,6 +27,7 @@ class SearchBar extends React.Component {
       <div className="SearchBar">
         <form onSubmit={this.onFormSubmit} className="input-group mb-3">
           <input 
+            ref={this.searchInputRef}
             type="text"
             value={this.state.term}
             onChange={e => this.setState({ term: e.target.value })}
