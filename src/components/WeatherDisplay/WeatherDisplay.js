c63f531e423a4a87f91b88bbe7685ae0ab5e3b22
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-16 17:09:34 +0100

    Added chatgpt autocomments

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 class WeatherDisplay extends React.Component {
   render() {
+    // Check if temperature and city props are not provided
     if (!this.props.temperature && !this.props.city) {
       return <div className="text-center">Enter a city to see the weather</div>;
     }
 
+    // Render weather information if temperature and city props are provided
     return (
       <div className="WeatherDisplay">
         <h2 className="text-center">The current temperature in {this.props.city} is {this.props.temperature}°C</h2>
