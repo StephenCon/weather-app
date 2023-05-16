ef47022daababf9610d1d509d4967e219ce60eed
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-16 17:09:02 +0100

    added ChatGPT autocomments

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -8,20 +8,20 @@ import './SearchBar.css';
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { term: '' };
+    this.state = { term: '' }; // Initializing the component state with an empty term
   }
 
   handleChange = (term) => {
-    this.setState({ term });
+    this.setState({ term }); // Update the term in the component state when the input value changes
   };
 
   handleSelect = (term) => {
-    geocodeByAddress(term)
-      .then(results => getLatLng(results[0]))
-      .then(latLng => console.log('Success', latLng))
-      .catch(error => console.error('Error', error));
-    this.setState({ term });
-    this.props.onSearch(term);
+    geocodeByAddress(term) // Geocode the selected address
+      .then(results => getLatLng(results[0])) // Get the latitude and longitude from the geocoded results
+      .then(latLng => console.log('Success', latLng)) // Log the latitude and longitude to the console
+      .catch(error => console.error('Error', error)); // Log any errors that occur during geocoding
+    this.setState({ term }); // Update the term in the component state with the selected address
+    this.props.onSearch(term); // Call the onSearch function passed as a prop with the selected address
   };
 
   render() {
@@ -29,20 +29,20 @@ class SearchBar extends React.Component {
       <div className="SearchBar">
         <div className="autocomplete-container">
           <PlacesAutocomplete
-            value={this.state.term}
-            onChange={this.handleChange}
-            onSelect={this.handleSelect}
+            value={this.state.term} // Set the value of the input field to the current term in the component state
+            onChange={this.handleChange} // Handle changes in the input field
+            onSelect={this.handleSelect} // Handle selection of an address from the autocomplete dropdown
           >
             {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
               <div>
                 <div className="autocomplete-dropdown-container">
-                  {loading && <div>Loading...</div>}
+                  {loading && <div>Loading...</div>} {/* Show a loading message while suggestions are being fetched */}
                   {suggestions.map(suggestion => {
                     const className = suggestion.active
-                      ? 'suggestion-item--active'
+                      ? 'suggestion-item--active' // Apply a CSS class to the active suggestion item
                       : 'suggestion-item';
                     const style = suggestion.active
-                      ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      ? { backgroundColor: '#fafafa', cursor: 'pointer' } // Apply a different background color to the active suggestion item
                       : { backgroundColor: '#ffffff', cursor: 'pointer' };
                     return (
                       <div
@@ -51,14 +51,14 @@ class SearchBar extends React.Component {
                           style,
                         })}
                       >
-                        <span>{suggestion.description}</span>
+                        <span>{suggestion.description}</span> {/* Display the suggestion description */}
                       </div>
                     );
                   })}
                 </div>
                 <input
                   {...getInputProps({
-                    placeholder: 'Enter city name...',
+                    placeholder: 'Enter city name...', // Set the placeholder text for the input field
                     className: 'form-control',
                   })}
                 />
@@ -67,7 +67,7 @@ class SearchBar extends React.Component {
           </PlacesAutocomplete>
         </div>
         <div className="input-group-append">
-          <button type="submit" className="btn btn-primary">Search</button>
+          <button type="submit" className="btn btn-primary">Search</button> {/* Render a search button */}
         </div>
       </div>
     );
