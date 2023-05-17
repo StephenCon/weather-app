b86f35098a609b6500f9ff3d5460ab5e9178fb81
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-17 11:41:52 +0100

    capitalised the first letter of the city

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,23 +1,20 @@
-// Importing necessary libraries
 import React from 'react';
 
-// Defining a functional component, WeatherDisplay, that takes two props: 'city' and 'temperature'
 const WeatherDisplay = ({ city, temperature }) => {
-  // The component returns a div which conditionally renders the city name and temperature
+  // Create a function to transform the first character to uppercase and remaining to lowercase
+  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
   return (
     <div>
-      {/* Conditional rendering: this block will only render if 'city' is not null or undefined */}
       {city && (
         <div className='p-4'>
-          {/* The name of the city is rendered within an h3 tag */}
-          <h3>Weather in {city}</h3>
-          {/* The temperature is rendered within an h3 tag, followed by a '°C' sign */}
-          <h3>{temperature}°C</h3>
+          {/* Call the capitalize function when rendering the city name */}
+          <h3>The weather in {capitalize(city)} is</h3>
+          <h1>{temperature}°C</h1>
         </div>
       )}
     </div>
   );
 }
 
-// Exporting the WeatherDisplay component so it can be imported and used in other components
 export default WeatherDisplay;
