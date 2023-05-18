8a1d2d94499003183061a59b87cb2b2a333d13cb
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-18 11:18:35 +0100

    added an if to make sure city isn't null

--- src/components/IconDisplay/IconDisplay.js
+++ src/components/IconDisplay/IconDisplay.js
@@ -9,16 +9,19 @@ function IconDisplay({ city }) {
     const apiKey = 'f023d6d413541c55d2f6cbcb018522e8';
 
     useEffect(() => {
-        axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then(response => {
-            setLoading(false);
-            setWeather(response.data.weather[0].icon);
-        })
-        .catch(error => {
-            setLoading(false);
-            setError(error.message);
-        });
+        if (city) {
+            axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+            .then(response => {
+                setLoading(false);
+                setWeather(response.data.weather[0].icon);
+            })
+            .catch(error => {
+                setLoading(false);
+                setError(error.message);
+            });
+        }
     }, [city]);
+    
 
     if (loading) {
         return <Spinner animation="border" />;
