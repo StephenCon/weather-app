01e55a6b9d40c3237d711864783cd7670de53f35
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-18 22:16:52 +0100

    Prettier

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import SearchBar from './components/SearchBar/SearchBar';
-import FetchWeather from './components/FetchWeather/FetchWeather';
-import DefaultLocation from './components/DefaultLocationHandler/DefaultLocationHandler';
-import './App.css';
-import IconDisplay from './components/IconDisplay/IconDisplay';
+import React from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+import FetchWeather from "./components/FetchWeather/FetchWeather";
+import DefaultLocation from "./components/DefaultLocationHandler/DefaultLocationHandler";
+import "./App.css";
+import IconDisplay from "./components/IconDisplay/IconDisplay";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // The state stores the city name
     this.state = {
-      city: ""
+      city: "",
     };
   }
 
   // This method updates the state when the location is determined
   handleLocationDetermined = (city) => {
     this.setState({ city });
-  }
+  };
 
   // This method updates the state when a city is searched
   handleSearch = (city) => {
     this.setState({ city });
-  }
+  };
 
   // Rendering the application's main components
   render() {
@@ -36,7 +36,7 @@ class App extends React.Component {
           <IconDisplay city={this.state.city} />
         </div>
         {/* DefaultWeather determines the default location when the application starts */}
-        <DefaultLocation onLocationDetermined={this.handleLocationDetermined} />        
+        <DefaultLocation onLocationDetermined={this.handleLocationDetermined} />
       </div>
     );
   }
