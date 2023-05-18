8960b811cdf41d75d2e193798c3756d454ae319b
Stephen Connett <StephenCon@users.noreply.github.com>
2023-05-18 11:28:24 +0100

    correct the capitals and commented

--- src/components/WeatherDisplay/WeatherDisplay.js
+++ src/components/WeatherDisplay/WeatherDisplay.js
@@ -1,20 +1,29 @@
-// Importing the necessary libraries
+// Import the React library.
 import React from 'react';
 
-// WeatherDisplay is a functional component that takes two props: 'city' and 'temperature'
+// The WeatherDisplay functional component.
+// This component takes in two props: 'city' and 'temperature'.
 const WeatherDisplay = ({ city, temperature }) => {
-  // This function capitalizes the first character of the string and makes the rest of the string lowercase
-  const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  
+  // The formatCity function.
+  // This function takes in a string, splits it on the comma, trims white spaces, 
+  // and converts the second part to uppercase. 
+  // It then joins these parts back together with a comma and returns the resulting string.
+  const formatCity = (str) => {
+    const parts = str.split(','); // Split the string on the comma
+    const formattedParts = [parts[0].trim(), parts[1].trim().toUpperCase()]; // Trim white spaces from the parts and convert the second part to uppercase
+    return formattedParts.join(', '); // Join the parts back together with a comma and return
+  };
 
-  // The component renders a div which conditionally displays the city name and temperature
+  // The component returns a 'div' element.
   return (
     <div>
-      {/* Conditional rendering: the following block will only render if 'city' is truthy (not null, undefined, or an empty string) */}
+      {/* If 'city' is not null or undefined, render another 'div' element. */}
       {city && (
         <div className='p-4'>
-          {/* The capitalized city name is displayed within an h3 tag */}
-          <h3>The weather in {capitalize(city)} is</h3>
-          {/* The temperature is displayed within an h1 tag, followed by the '°C' sign */}
+          {/* Render an 'h3' element with the formatted city name. */}
+          <h3>{formatCity(city)}:</h3>
+          {/* Render an 'h1' element with the temperature. */}
           <h1>{temperature}°C</h1>
         </div>
       )}
@@ -22,5 +31,5 @@ const WeatherDisplay = ({ city, temperature }) => {
   );
 }
 
-// The WeatherDisplay component is exported so that it can be imported and used in other components
+// Export the WeatherDisplay component as the default export.
 export default WeatherDisplay;
