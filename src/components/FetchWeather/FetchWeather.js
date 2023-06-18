9c62a05acff7b222862296867ef1d29f20ad0a3f
Stephen Connett <StephenCon@users.noreply.github.com>
2023-06-18 14:55:18 +0100

    commented code

--- src/components/FetchWeather/FetchWeather.js
+++ src/components/FetchWeather/FetchWeather.js
@@ -1,33 +1,40 @@
+// Import necessary libraries and components
 import React from "react";
 import WeatherDisplay from "../WeatherDisplay/WeatherDisplay";
 import { fetchWeather } from "../../services/weatherservice";
 
+// Create a class component named FetchWeather
 class FetchWeather extends React.Component {
+  // Define the constructor method, initializing state with temperature and error properties
   constructor(props) {
     super(props);
     this.state = {
       temperature: "",
-      error: null, // add an error field to the state
+      error: null, // Initialize error as null, this will store any potential error message
     };
   }
 
+  // Use the componentDidUpdate lifecycle method to update the component when props change
   componentDidUpdate(prevProps) {
     if (this.props.city !== prevProps.city) {
-      this.setState({ error: null }); // reset the error state when a new city is searched
-      this.fetchWeather();
+      this.setState({ error: null }); // Reset the error state when a new city is searched
+      this.fetchWeather(); // Call fetchWeather function whenever city prop changes
     }
   }
 
+  // Define the method fetchWeather to get the temperature data for a given city
   fetchWeather = () => {
-    fetchWeather(this.props.city)
+    fetchWeather(this.props.city) // Use fetchWeather function imported from weatherservice module
       .then((data) => {
+        // On successful fetch, set the temperature state to the received data and reset error state
         this.setState({
           temperature: data.temperature,
-          error: null, // set the error state to null when fetch is successful
+          error: null,
         });
       })
-      .catch((error) => { // handle fetch error
+      .catch((error) => { // Catch any errors occurred during fetching
         console.error(error);
+        // In case of error, reset temperature and set error state with an error message
         this.setState({
           temperature: "",
           error: "Failed to fetch weather data. Please try again with a valid city name.",
@@ -35,17 +42,21 @@ class FetchWeather extends React.Component {
       });
   };
 
+  // Define the render method for the component
   render() {
     return (
       <div>
+        {/* Render the WeatherDisplay component, passing it the current city and temperature as props */}
         <WeatherDisplay
           city={this.props.city}
           temperature={this.state.temperature}
         />
+        {/* Conditionally render an error message if there's an error */}
         {this.state.error && <div className="error-message">{this.state.error}</div>}
       </div>
     );
   }
 }
 
+// Export the FetchWeather component so it can be used in other components
 export default FetchWeather;
