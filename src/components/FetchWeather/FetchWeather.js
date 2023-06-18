cb65444de715cc763292145ed566d3158a96bb8e
Stephen Connett <StephenCon@users.noreply.github.com>
2023-06-18 14:52:32 +0100

    fetch weather now doesn't crash when error thrown

--- src/components/FetchWeather/FetchWeather.js
+++ src/components/FetchWeather/FetchWeather.js
@@ -1,51 +1,51 @@
-// Importing necessary libraries and components
 import React from "react";
 import WeatherDisplay from "../WeatherDisplay/WeatherDisplay";
 import { fetchWeather } from "../../services/weatherservice";
 
-// Defining a class-based component, FetchWeather, that extends the base React Component class
 class FetchWeather extends React.Component {
-  // Constructor method for the WeatherContainer component
   constructor(props) {
     super(props);
-    // Initializing the component's state with an empty temperature
     this.state = {
       temperature: "",
+      error: null, // add an error field to the state
     };
   }
 
-  // Method called automatically by React whenever a component's props or state changes
   componentDidUpdate(prevProps) {
-    // If the city prop has changed
     if (this.props.city !== prevProps.city) {
-      // Fetch the weather for the new city
+      this.setState({ error: null }); // reset the error state when a new city is searched
       this.fetchWeather();
     }
   }
 
-  // Method to fetch the weather for a given city
   fetchWeather = () => {
-    // Calling the fetchWeather function imported from the weatherservice module
     fetchWeather(this.props.city)
-      // When the promise resolves, updating the component's state with the new temperature
       .then((data) => {
         this.setState({
           temperature: data.temperature,
+          error: null, // set the error state to null when fetch is successful
+        });
+      })
+      .catch((error) => { // handle fetch error
+        console.error(error);
+        this.setState({
+          temperature: "",
+          error: "Failed to fetch weather data. Please try again with a valid city name.",
         });
       });
   };
 
-  // Render method to define what the WeatherContainer component should render
   render() {
-    // Render the WeatherDisplay component, passing it the current city and temperature as props
     return (
-      <WeatherDisplay
-        city={this.props.city}
-        temperature={this.state.temperature}
-      />
+      <div>
+        <WeatherDisplay
+          city={this.props.city}
+          temperature={this.state.temperature}
+        />
+        {this.state.error && <div className="error-message">{this.state.error}</div>}
+      </div>
     );
   }
 }
 
-// Exporting the WeatherContainer component so it can be imported and used in other components
 export default FetchWeather;
