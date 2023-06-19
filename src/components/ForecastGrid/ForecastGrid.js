ef733a8cf7e01603f162a33ca6219e8afcb33a02
Stephen Connett <StephenCon@users.noreply.github.com>
2023-06-19 11:31:43 +0100

    added comments

--- src/components/ForecastGrid/ForecastGrid.js
+++ src/components/ForecastGrid/ForecastGrid.js
@@ -1,10 +1,11 @@
 // Import necessary modules
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
-import './ForecastGrid.css';
+import React from 'react';  // Import React library for building user interfaces
+import { Container, Row, Col } from 'react-bootstrap';  // Import necessary components from react-bootstrap library for layout
+import './ForecastGrid.css';  // Importing external CSS file
 
 // Define ForecastGrid as a functional component
 const ForecastGrid = () => {
+
     // Define an array of days of the week
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -16,11 +17,13 @@ const ForecastGrid = () => {
 
     // Define a function to calculate the next seven days
     const getNextSevenDays = () => {
+
         // Initialize an empty array to store the names of the next seven days
         let nextDays = [];
 
         // Loop from 1 to 7 (inclusive)
         for (let i = 1; i <= 7; i++) {
+
             // Calculate the index of the next day by adding i to today and taking the modulus 7
             // This ensures that the index wraps around to 0 (Sunday) after 6 (Saturday)
             // Then push the name of the next day to the nextDays array
@@ -38,11 +41,16 @@ const ForecastGrid = () => {
     return (
         // Create a container using the Container component from react-bootstrap
         <Container>
+
             <Row>
                 {nextSevenDays.map((day, index) => (
+                    // For each day in the next seven days, create a new Col component
+                    // Each Col component is given a unique key (its index), and some CSS classes for styling
+                    // Inside each Col component, the name of the day is displayed
                     <Col key={index} className="shadow p-3 m-2 rounded">{day}</Col>
                 ))}
             </Row>
+
         </Container>
     );
 };
