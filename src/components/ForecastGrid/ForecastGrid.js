9de660d05837cd42913ef4116b9f63eeb7bd6808
Stephen Connett <StephenCon@users.noreply.github.com>
2023-06-20 18:29:44 +0100

    removed padding and margin

--- src/components/ForecastGrid/ForecastGrid.js
+++ src/components/ForecastGrid/ForecastGrid.js
@@ -47,7 +47,7 @@ const ForecastGrid = () => {
                     // For each day in the next seven days, create a new Col component
                     // Each Col component is given a unique key (its index), and some CSS classes for styling
                     // Inside each Col component, the name of the day is displayed
-                    <Col key={index} className="shadow p-4 m-3 rounded">{day}</Col>
+                    <Col key={index} className="shadow rounded">{day}</Col>
                 ))}
             </Row>
 
