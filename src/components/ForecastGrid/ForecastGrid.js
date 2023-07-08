db83ae0f1a3b611ad5c7893efc4063380aca0064
Stephen Connett <StephenCon@users.noreply.github.com>
2023-07-08 15:25:55 +0100

    Weather Icon now displays

--- src/components/ForecastGrid/ForecastGrid.js
+++ src/components/ForecastGrid/ForecastGrid.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { fetchForecast } from '../../services/weatherservice';
+import DisplayWeatherIcon from '../DisplayWeatherIcon/DisplayWeatherIcon';
 import './ForecastGrid.css';
 
 const ForecastGrid = ({ city }) => {
@@ -11,15 +12,21 @@ const ForecastGrid = ({ city }) => {
         if (city) {
             fetchForecast(city)
                 .then(forecastData => {
-                    // Process dates and filter out today's forecast, keep next 5 days only
                     const today = new Date();
-                    today.setHours(0, 0, 0, 0);  // Normalize to mid-night today
-
-                    const nextFiveDaysForecast = forecastData
-                        .map(item => ({ ...item, date: new Date(item.date) }))  // Ensure dates are Date objects
-                        .filter(item => item.date.getTime() > today.getTime())  // Filter out today's data
-                        .slice(0, 5);  // Keep next 5 days only
-
+                    today.setHours(0,0,0,0); // Set the time to 00:00:00
+                    const dateMap = new Map();
+    
+                    forecastData
+                        .map(item => ({ ...item, date: new Date(item.date) }))
+                        .forEach(item => {
+                            item.date.setHours(0,0,0,0); // Set the time to 00:00:00
+                            const key = item.date.toDateString();
+                            if (!dateMap.has(key) && item.date.getTime() >= today.getTime()) {
+                                dateMap.set(key, item);
+                            }
+                        });
+    
+                    const nextFiveDaysForecast = Array.from(dateMap.values()).slice(0, 5);
                     setForecast(nextFiveDaysForecast);
                 })
                 .catch(error => {
@@ -32,15 +39,13 @@ const ForecastGrid = ({ city }) => {
         <Container>
             <Row>
                 {forecast.map((dayForecast, index) => {
-                    // Create a new Date object from the date string
                     const date = new Date(dayForecast.date);
-
-                    // Use getDay() to get the day of the week as a number, and map it to a name
                     const dayName = days[date.getDay()];
 
                     return (
                         <Col key={index} className="forecast-boxes shadow rounded p-4 m-4">
                             <p>{dayName}</p>
+                            <DisplayWeatherIcon weather={dayForecast.icon} />
                             <p>Temperature: {dayForecast.temperature}°C</p>
                             <p>Weather: {dayForecast.weather}</p>
                         </Col>
